fdec001078a0afa47704628c1800a174fd347691
SujalChhetriKarki <Xetrisujal2003@gmail.com>
2025-12-11 14:33:26 +0545

    yes

---
+++ W1/1_datatype.js
@@ -0,0 +1,13 @@
+let x = "Sujal" + "16";
+console.log(x);
+console.log(typeof x); 
+
+let a=5;
+let b=5;
+console.log(a==b)
+
+let s=["Sujal","Suyog"];
+console.log(s);
+
+let z = "sujal";
+console.log(typeof z);
